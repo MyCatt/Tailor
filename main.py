ed4078e99e82c6735bc71f99ff407da45cd96595
Michael Catterall <43209825+MyCatt@users.noreply.github.com>
2024-02-11 22:24:13 +1300

    Better prompt

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import os
+
+import requests
+
 import auth
 from github import Auth, Github
 from openai import OpenAI
@@ -17,22 +20,44 @@ def find_articles():
         path = content_file.path
         file_name = path.replace("articles/fin-ops-core/fin-ops/get-started/", "")
         if "whats-new-platform-updates" in file_name:
+
+            os.mkdir("./Articles/" + file_name[:-3])
+            os.mkdir("./Articles/" + file_name[:-3] + "/transcript")
+
+            # Get the article content from git
             article = repo.get_contents(path)
+
+            # Summarise the article with OpenAI
             article_content = summarise_article(article)
+
+            # Generated a thumbnail using the article generated above
             thumbnail_prompt = create_thumbnail(article_content)
-            thumbnail = generated_thumbnail(thumbnail_prompt)
-            save_article(file_name, article_content, thumbnail)
+            # Generate a thumbnail using the prompt above
+            thumbnail = generated_thumbnail(file_name[:-3], thumbnail_prompt)
+
+            # Generate the audio transcript
+            transcript = summarise_article_audio(article_content)
+
+            # Save the article to a file
+            save_article(file_name, article_content, thumbnail, transcript)
+
+            # Generate and save the audio file
+            generate_audio(file_name[:-3], transcript)
+
             return
 
     # To close connections after use
     g.close()
 
 
-def save_article(file_name, article_content, thumbnail):
-    with open("Articles/" + file_name, 'w', encoding="utf-8") as f:
+def save_article(file_name, article_content, thumbnail, transcript):
+    with open("Articles/" + file_name[:-3] + "/" + file_name, 'w', encoding="utf-8") as f:
         f.write(article_content)
         f.write("\n\n![Thumbnail](" +thumbnail + ")")
         f.write('\n[Full Article] ' + "https://learn.microsoft.com/en-us/dynamics365/fin-ops-core/fin-ops/get-started/" + file_name)
+
+    with open("Articles/" + file_name[:-3] + "/transcript/" + file_name, 'w', encoding="utf-8") as f:
+        f.write(transcript)
 
 
 def summarise_article(article):
@@ -40,10 +65,9 @@ def summarise_article(article):
         messages=[
             {
                 "role": "user",
-                "content": "Write an honest and engaging Linked In post summarising the following update release notes. The readers of this are less technical users that want to stay up to date. "
-                           "You do not work for this company, you're just creating an easier way of reading it."
-                           "This article should have a corporate style"
-                            "Write a catchy relevant title"
+                "content": "Write an honest and engaging Linked In post summarising the following update release notes."
+                           "You do not work for this company, you're just creating an educational post."
+                           "This article should have a strictly corporate style"
                            "The input (follows) and output are both markdown."
                            + str(article),
             }
@@ -53,6 +77,21 @@ def summarise_article(article):
     print(chat_completion)
     content = chat_completion.choices[0].message.content
     print(content)
+    return content
+
+
+def summarise_article_audio(article):
+    chat_completion = c.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": "take this article and create an audio transcript that I can supply to the openAI speech API. This transcript should only include words that should be read and spoken directly."
+                           + str(article),
+            }
+        ],
+        model="gpt-4-0125-preview",
+    )
+    content = chat_completion.choices[0].message.content
     return content
 
 
@@ -72,7 +111,7 @@ def create_thumbnail(article):
     return content
 
 
-def generated_thumbnail(context):
+def generated_thumbnail(file_name, context):
     image_completion = c.images.generate(
         model="dall-e-3",
         prompt= context + ". Characteristics: Mandatory colors: [#7F27FF, #FF8911], gradient, dimensional graphic design, Illusion of live-like depth and volume, Employs various lighting effects, Shadow and depth indications often utilise one colour, with tonal variations",
@@ -82,8 +121,21 @@ def generated_thumbnail(context):
         style="vivid"
     )
     image_url = image_completion.data[0].url
+    img_data = requests.get(image_url).content
+    with open("Articles/" + file_name + "/" + file_name +'.jpg', 'wb') as handler:
+        handler.write(img_data)
     return image_url
 
+
+def generate_audio(file_name, context):
+    speech_file_path = "Articles/" + file_name + "/" + file_name + ".mp3"
+    response = c.audio.speech.create(
+        model="tts-1",
+        voice="alloy",
+        input=context
+    )
+
+    response.stream_to_file(speech_file_path)
 
 
 def authenticate_git():
